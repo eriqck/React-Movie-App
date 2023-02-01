7fb91c3868b1eb008ae7cd6d6d7fedc53e56309d
eriqck <ericdavid348@gmail.com>
2023-02-01 16:05:05 +0300

    useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
+import { useState, useEffect } from 'react';
 import './App.css';
-
-const Person = (props) => {
-  return (
-    <>
-
-      <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.LastName}</h2>
-      <h2>Age: {props.Age}</h2>
-    </>
-  )
-}
 
 const App = () => {
 
+  const [counter, setcounter] = useState(0);
+  useEffect(() => {
+    alert('Youve changed the counter to '  + counter);
+    
+  }, [counter])
   
   return (
     <div className="App">
-      <Person name='john' LastName='Doe' Age={'25'}/>
-      <Person name='Mary' LastName='Doe' Age={'35'}/>
-      
+      <button onClick={() => setcounter((prevCount) => prevCount - 1)}>-</button>  
+      <h1>{counter}</h1>
+      <button onClick={() => setcounter((prevCount) => prevCount + 1)}>+</button>
 
-
-    
     </div>
   );
 }
